d2a4e171f3414736e8a868355247bde37afe7b20
wellintons <wellinton619@gmail.com>
2025-05-22 13:20:00 -0400

    created

---
+++ src/test/java/com/devsuperior/spring_intensivao/SpringIntensivaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.spring_intensivao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntensivaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
